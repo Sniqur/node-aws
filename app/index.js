2bc8c5a0f62c3219e30d19100f9793dfb78d9341
Stefan <stephansnigur@gmail.com>
2024-04-18 17:55:57 +0300

    fixedUnexpectedToken

--- app/index.js
+++ app/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const PORT  = process.env.APP_PORT ?? 3000
+const PORT  = process.env.APP_PORT ?  process.env.APP_PORT : 3000;
 app.get('/', function (req, res) {
     res.send('Hello World')
 })
